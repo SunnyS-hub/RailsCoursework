507f8955f86c1a10590e35bb9ff3c3f7e092cf52
SunnyS-hub <ss02249@surrey.ac.uk>
2019-12-06 13:19:16 +0000

    functionality and views for editing books and index action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,10 +4,37 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def index
+    @allBooks = Book.all
+  end
+
   def create
     @book = Book.new(book_params)
-    @book.save
-    redirect_to books_show(@article)
+    if @book.save
+      flash[:notice] = "The book was successfully added to your collection"
+      redirect_to book_path(@book)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "The book information was successfully updated"
+      redirect_to book_path(@book)
+    else
+      render 'edit'
+    end
   end
 
 
