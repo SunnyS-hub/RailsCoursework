7f85331e819b8e1b9fca3f1bd8d50211149e02fd
SunnyS-hub <ss02249@surrey.ac.uk>
2019-12-14 17:54:50 +0000

    still styling devise forms. Added links to user form on home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
+  after_initialize :set_default
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+       :recoverable, :rememberable, :validatable
+
+  #Only make the first user admin
+  def set_default
+    if User.count == 0
+      self.admin = true
+    end
+  end
+
 end
